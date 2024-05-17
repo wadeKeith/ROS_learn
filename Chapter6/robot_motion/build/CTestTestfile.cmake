0050ad434bc45e649efe027e0c6d1a964f288a46
wadeKeith <2470887663@qq.com>
2024-05-18 00:22:32 +0800

    nothing

---
+++ Chapter6/robot_motion/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/yin/Documents/Github/ROS_learn/Chapter6/robot_motion/src
+# Build directory: /home/yin/Documents/Github/ROS_learn/Chapter6/robot_motion/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
