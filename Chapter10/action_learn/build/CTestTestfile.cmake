4aa1e26398be9b405b231cd9e72256c1586f5398
wadeKeith <2470887663@qq.com>
2024-05-19 12:22:45 +0800

    nothing

---
+++ Chapter10/action_learn/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/yin/Documents/Github/ROS_learn/Chapter10/action_learn/src
+# Build directory: /home/yin/Documents/Github/ROS_learn/Chapter10/action_learn/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ Chapter10/action_learn/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/yin/Documents/Github/ROS_learn/Chapter10/action_learn/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/usr/local/cuda-12.4/lib64'
+export PATH='/opt/ros/noetic/bin:/home/yin/.vscode-server/cli/servers/Stable-dc96b837cf6bb4af9cd736aa3af08cf8279f7685/server/bin/remote-cli:/home/yin/anaconda3/condabin:/usr/local/cuda-12.4/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/opt/MATLAB/R2024a/bin'
+export PWD='/home/yin/Documents/Github/ROS_learn/Chapter10/action_learn/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/yin/Documents/Github/ROS_learn/Chapter10/action_learn/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/yin/Documents/Github/ROS_learn/Chapter10/action_learn/src:$ROS_PACKAGE_PATH"
